200279efa690f35659b98b09674df110bbda798f
Timothy Mihet <71294550+mihet101@users.noreply.github.com>
2021-02-12 22:28:52 -0500

    Update testing.py

--- testing.py
+++ testing.py
@@ -11,12 +11,5 @@ A = list(map(int,input("\nEnter the numbers: ").strip().split()))
 print("\nstarting array: ", A)
 result = merge_insertion_sort(A,tree_level)
 
-length = len(A)
-for i in range(1,tree_level):
-    if i == 1:
-        print("\t"*3, A)
-    else: 
-        length = len(A)/2
-
 print()
 print("Sorted List: ", result)
